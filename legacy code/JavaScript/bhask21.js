2d0bc99451a3faac3bf3ce151dc4e5a30bb1d236
Israel Santana <israelsantanamorais@outlook.com>
2024-07-27 16:20:41 -0300

    commit five

--- legacy code/JavaScript/bhask21.js
+++ legacy code/JavaScript/bhask21.js
@@ -1,20 +1,24 @@
 function teste() {
-    delta = document.getElementById("delta")
-    a = document.getElementById("a")
-    b = document.getElementById("b")
-    c = document.getElementById("c")
-    x1 = document.getElementById("x1")
-    x2 = document.getElementById("x2")
-    a = parseFloat(a.value)
-    b = parseFloat(b.value)
-    c = parseFloat(c.value)
-    x1 = parseFloat(x1.value)
-    x2 = parseFloat(x2.value)
+    var a = parseFloat(document.getElementById("a").value);
+    var b = parseFloat(document.getElementById("b").value);
+    var c = parseFloat(document.getElementById("c").value);
 
-    d = (b * b) - (4 * a * c)
-    delta.value = d
-    x1 = (-b + Math.sqrt(d)) / (2 * a)
-    x2 = (-b - Math.sqrt(d)) / (2 * a)
-    xp.value = x1
-    xs.value = x2
+    var delta = b * b - (4 * a * c);
+
+    if (delta < 0) {
+        document.getElementById('res').innerHTML = "No real roots (Delta < 0)";
+        document.getElementById('x1').innerHTML = "";
+        document.getElementById('x2').innerHTML = "";
+    } else if (delta > 0) {
+        var x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        var x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        document.getElementById('res').innerHTML = "There are two roots:";
+        document.getElementById('x1').innerHTML = `x1 = ${x1}`;
+        document.getElementById('x2').innerHTML = `x2 = ${x2}`;
+    } else {
+        var x = -b / (2 * a);
+        document.getElementById('res').innerHTML = `There is one root: x = ${x}`;
+        document.getElementById('x1').innerHTML = "";
+        document.getElementById('x2').innerHTML = "";
+    }
 }
